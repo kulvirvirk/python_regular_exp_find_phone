75d5f1a461e4160cb1682afe9ccf782cebb19868
kulvirvirk <kvirkdev@gmail.com>
2020-04-28 05:07:00 +0000

    defined regex for phone_num

--- main.py
+++ main.py
@@ -5,3 +5,7 @@
 import re
 
 some_string_with_ph_num = "Hey, call me on 212-555-6363!"
+
+#create a reg ex object to store phone_num_regex variable; use re.compile function
+#note: make sure to use raw string by using r in front
+phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
