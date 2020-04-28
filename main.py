92a077f4b58deaba5a512d2f7df4d34560db19e0
kulvirvirk <kvirkdev@gmail.com>
2020-04-28 05:10:48 +0000

    saved the searched string in match_object

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ some_string_with_ph_num = "Hey, call me on 212-555-6363!"
 #create a reg ex object to store phone_num_regex variable; use re.compile function
 #note: make sure to use raw string by using r in front
 phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
+# save the searched string in match_object variable
+match_object = phone_num_regex.search(some_string_with_ph_num)
