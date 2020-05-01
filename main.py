b4fd2a2d31414d6fa68c6c266e04419e9820f619
kulvirvirk <kvirkdev@gmail.com>
2020-05-01 05:55:38 +0000

    created regex to find phone number with optional area code

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # 2. use regex to find a word that starts with 'Bat' from a given string
 # note: use a pipe to separate the options
 
+# 3. find the phone number, where area code is optional.
+
 
 # 1. use regex - find a valid phone number from a given string
 # Import the re module
@@ -26,3 +28,12 @@ print('=====================================')
 bat_regex = re.compile(r'Bat(man|mobile|copter)')
 match_object = bat_regex.search('Batmobile lost a wheel!')
 print(match_object.group())
+print('=====================================')
+
+
+
+# 3. find the phone number, where area code is optional
+
+# create a regex 
+phone_regex = re.compile('(\d\d\d-)?\d\d\d-\d\d\d\d')
+match_object_1 = phone_regex.search(some_string_with_ph_num)
